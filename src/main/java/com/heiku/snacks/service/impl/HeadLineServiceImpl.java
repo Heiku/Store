0fa848e003bb96e0a05a56ae9669ad1f727063a7
Heiku <1154034741@qq.com>
2018-05-30 22:17:43 +0800

    init headline dao & service

---
+++ src/main/java/com/heiku/snacks/service/impl/HeadLineServiceImpl.java
@@ -0,0 +1,22 @@
+package com.heiku.snacks.service.impl;
+
+import com.heiku.snacks.dao.HeadLineDao;
+import com.heiku.snacks.entity.HeadLine;
+import com.heiku.snacks.service.HeadLineService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class HeadLineServiceImpl implements HeadLineService {
+
+    @Autowired
+    private HeadLineDao headLineDao;
+
+    @Override
+    public List<HeadLine> getHeadLineList(HeadLine headLineCondition) {
+        return headLineDao.queryHeadLine(headLineCondition);
+    }
+}
