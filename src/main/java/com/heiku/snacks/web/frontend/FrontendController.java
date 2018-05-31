576bbf4b0de485cc0d1ac33f179a31927b0b3432
Heiku <1154034741@qq.com>
2018-05-31 21:39:18 +0800

    change

---
+++ src/main/java/com/heiku/snacks/web/frontend/FrontendController.java
@@ -0,0 +1,31 @@
+package com.heiku.snacks.web.frontend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/frontend")
+public class FrontendController {
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    private String index(){
+        return "frontend/index";
+    }
+
+
+    @RequestMapping(value = "myrecord", method = RequestMethod.GET)
+    private String myRecord(){
+        return "frontend/myRecord";
+    }
+
+    @RequestMapping(value = "mypoint", method = RequestMethod.GET)
+    private String myPoint(){
+        return "frontend/myPoint";
+    }
+
+    @RequestMapping(value = "pointRecord", method = RequestMethod.GET)
+    private String pointRecord(){
+        return "frontend/pointRecord";
+    }
+}
